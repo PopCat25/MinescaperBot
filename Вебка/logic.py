c7477eae098efdd8fd2cd75426dd6b26db7939c2
ProffesionalProgramerWith3YearsOld <80975318+ProffesionalProgramerWith3YearsOld@users.noreply.github.com>
2022-11-23 09:09:20 +0500

    Add files via upload
    В основном переписана логика (logic.py) код работающий с сайтом и парсящий с него данные вынесен в отдельный файл (siteWorker), добавлено отображение числа поражений и побед, теперь перезапуск игры при победе или поражении

--- Вебка/logic.py
+++ Вебка/logic.py
@@ -1,7 +1,7 @@
 from selenium import *
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from cellParser import *
+from siteWorker import *
 import time
 import os
 
@@ -12,7 +12,7 @@ def start(difLevel):
         driver.get(url=url)
         time.sleep(3)  # Ожидаем загрузки страницы
     
-        h,w = 0,0
+        h,w = 0,0           #Создание  переменных высоты и ширины поля в зависимости от выбранной сложности
         if difLevel == 1 :
             h = 9
             w = 9
@@ -23,43 +23,23 @@ def start(difLevel):
             h = 16
             w = 30
 
-        field = [[0] * w for i in range(h)]
+        win,lose = 0,0
+        while True:
 
-        reset = driver.find_element(By.XPATH, "//*[@id=\"top_area_face\"]")
-        continued = True
+            mineCount = mineCountPars(driver) # Вызываем функцию из сайтворкера, шоб кол-во мин узнать
+            field = gameFieldPars(h,w,driver) # Вызываем функцию из сайтворкера для парсинга игрового поля
 
-        while continued:
+            os.system('CLS')    # Очищение консоли
             
-            mineCount = 0
-            for i in (1,10,100):
-                for j in filter (str.isdigit ,driver.find_element(By.XPATH, f"//*[@id=\"top_area_mines_{i}\"]").get_attribute("class")):
-                    mineCount += i * int(j)
-
-            for j in range (h): 
-                for i in range (w):
-                    element = driver.find_element(By.XPATH, f"//*[@id=\"cell_{i}_{j}\"]")
-                    elementClassLine = element.get_attribute("class").split()                    
-                    field[j][i] = cellParser(elementClassLine)
-                #     print(elementClassLine,i,j)
-                # print("Строчка кончилась")
-            
-            os.system('CLS')
-            print(f"Осталось мин: {mineCount}")
+            print(f"Осталось мин: {mineCount}") 
             for row in field:
                 print(row)
 
 
             
+            win,lose = checkGameConsist(driver,win,lose) #помимо ресета в случае конца игры ещё делаем +1 в счётчикам винов\лузов
+            print(f"Побед:{win} Поражений: {lose}")
 
-
-            if reset.get_attribute("class") == "top-area-face zoomable hd_top-area-face-lose":
-                # print(reset.get_attribute("class"))
-                reset.click()
-                # continued = False
-            elif reset.get_attribute("class") == "top-area-face zoomable hd_top-area-face-win":
-                # print(reset.get_attribute("class"))
-                continued = False
-            # print(reset.get_attribute("class"))
 
 
     except Exception as ex :
